86a3e13a06b5292e8420a91714ff7e94656478a5
Cesar Augusto <cesar.leitejm@hotmail.com>
2021-09-01 15:59:57 -0300

    Teste pull request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -125,6 +125,16 @@ class ContatoController extends Controller
         }
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
     /**
      * Show the form for editing the specified resource.
